acc8be02c1fc46421b3d8197b552fbae8db4b330
Mayank Garg <gargmayank0505@gmail.com>
2023-08-24 19:16:03 +0530

    modify checker.sh

--- checker.sh
+++ checker.sh
@@ -1,6 +1,9 @@
 g++ -std=c++17  correct_solution.cpp -o correct_solution
 g++ -std=c++17  incorrect_solution.cpp -o incorrect_solution
 g++ -std=c++17  test_case_generator.cpp -o test_case_generator
+if [ -f output.txt ]; then
+    > output.txt  # Clear the file if it exists
+fi
 
 for((i=1; ; ++i));do
     ./test_case_generator $i >randomInput
@@ -8,6 +11,10 @@ for((i=1; ; ++i));do
     ./correct_solution <randomInput>  correctOutput
     diff -w myOutput correctOutput || break
     echo "Passed test: " $i
+    echo "Passed test: $i" >> output.txt
+    cat randomInput >> output.txt
+    cat myOutput >> output.txt
+
     # cat randomInput
     # cat myOutput
 
@@ -19,3 +26,10 @@ echo "your ans is : "
 cat myOutput
 echo "correct ans is: "
 cat correctOutput
+
+echo "\nWA on the following test:" >> output.txt
+cat randomInput >> output.txt
+echo "your ans is : " >> output.txt
+cat myOutput >> output.txt
+echo "correct ans is : " >> output.txt
+cat correctOutput >> output.txt
